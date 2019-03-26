ba71adbc55b5c0aa7cbd812a5789b6559418db42
Catarina Machado <catarinamachado11@gmail.com>
2019-03-26 19:56:44 +0000

    Add prettier html

--- Transformer/include/Angola.h
+++ Transformer/include/Angola.h
@@ -7,14 +7,15 @@ typedef struct angola *Angola;
 #define STOP_TAGGING trace()
 
 Angola mkAngola();
-void unmkAngola();
+void unmkAngola(Angola a);
+void printHTML(Angola a);
 
 void addId(Angola a, char* str);
 void addAuthor(Angola a, char* str);
 void addTag(Angola a, char* str);
 void addTitle(Angola a, char* str);
 void addCategory(Angola a, char* str);
-void addText(Angola a, char* str);
+void addLine(Angola a, char *str);
 
 void tag();
 void trace();
--- Transformer/src/Angola.c
+++ Transformer/src/Angola.c
@@ -10,7 +10,7 @@ typedef struct angola {
     GList * tags;
     GString * title;
     GString * category;
-    GString * text;
+    GList * lines;
 } *Angola;
 
 typedef struct tuple {
@@ -25,37 +25,36 @@ Tuple mkTuple(GString * id, GString * title){
     return t;
 }
 
-void unmkTuple( Tuple t ){
+void unmkTuple(Tuple t){
    g_string_free(t->id, TRUE);
    g_string_free(t->title, TRUE);
    g_free(t);
 }
 
-void stringTotalFree( GString * str){
+void stringTotalFree(GString * str){
     g_string_free(str, TRUE);
 }
 
-void addlink(GString * tag, GString* id,GString * title ){
-    
+void addlink(GString * tag, GString* id, GString * title ){
     GString *a, *b;
     GHashTable *l,*tmp;
     Tuple t;
 
-    if( g_hash_table_contains(taghtable,tag) ){
+    if(g_hash_table_contains(taghtable,tag)){
         l = (GHashTable *)g_hash_table_lookup(taghtable,tag);
         t = mkTuple(id,title);
         g_hash_table_insert(l,t->id,t);
 
         g_hash_table_steal_extended(taghtable,tag, &a, &tmp);
         g_hash_table_insert(taghtable,a,l);
-    }else{
+    } else {
         l = g_hash_table_new_full(g_string_hash,g_string_equal,NULL,unmkTuple);
         a = g_string_new(tag->str);
         t = mkTuple(id,title);
         g_hash_table_insert(l,t->id,t);
         g_hash_table_insert(taghtable,a,l);
     }
-    
+
 }
 
 Angola mkAngola(){
@@ -65,55 +64,104 @@ Angola mkAngola(){
     a->author = g_string_new("");
     a->title = g_string_new("");
     a->category = g_string_new("");
-    a->text = g_string_new("");
     a->tags = NULL;
+    a->lines = NULL;
     return a;
 }
 
 void unmkAngola(Angola a){
-    GList *l;
-    char * tmp;
-    char * id = a->id->str;
-    char * author = a->author->str;
-    char * title = a->title->str;
-    char * category = a->category->str;
-    char * text = a->text->str; 
-
-    printf("<pub id=\"%s\">\n",id);
-
-    if(strstr(title,"---------"))
-    {
-        printf("<title></title>\n");
-    }else{
-        printf("<title>%s</title>\n",title);
-    }
-
-    printf("<author_date>%s</author_date>\n",author);
-    printf("\t<tags>\n\t\t");
-    for (l = a->tags; l != NULL; l = l->next)
-    {
-        addlink(l->data,a->id,a->title);
-        tmp = g_string_free(l->data,FALSE);
-        printf("<tag>%s</tag> ",tmp);
-        g_free(tmp);
-    }
-    printf("\n\t</tags>\n");
-    printf("\t<category>%s</category>\n",category);
-    
-    if( text[0]=='\n' )
-        printf("<text>%s\n</text>\n",text);
-    else
-        printf("<text>\n%s\n</text>\n",text);
-
-    printf("</pub>\n");
-
     g_string_free(a->id,TRUE);
     g_string_free(a->author,TRUE);
     g_string_free(a->title,TRUE);
     g_string_free(a->category,TRUE);
-    g_string_free(a->text,TRUE);
     g_list_free(a->tags);
+    g_list_free(a->lines);
     g_free(a);
+}
+
+void printHTML(Angola a){
+    GList *l;
+    char *tmp;
+    char *id = a->id->str;
+    char *author = a->author->str;
+    char *title = a->title->str;
+    char *category = a->category->str;
+
+    printf("<pub id=\"%s\">\n", id);
+
+    printf("<!DOCTYPE html>\n");
+    printf("<html>\n");
+    printf("<head>\n");
+
+    if (strstr(title, "---------"))
+    {
+        printf("<title></title>\n");
+    }
+    else
+    {
+        printf("<title>%s</title>\n", title);
+    }
+
+    printf("</head>\n");
+    printf("<body>\n");
+
+    printf("<h1><p align='center'><font color='#2874A6'>%s</font></p></h1>\n", title);
+
+    printf("<div style='padding: 20px'>\n");
+    printf("<author_date><h3><font color='#85C1E9'>%s</font></h3></author_date>\n", author);
+
+    printf("<p><tags><b>Tags:</b> \n");
+    for (l = a->tags; l != NULL; l = l->next)
+    {
+        addlink(l->data, a->id, a->title);
+        tmp = g_string_free(l->data, FALSE);
+        printf("<tag>#%s</tag> \n", tmp);
+        g_free(tmp);
+    }
+    printf("</tags></p>\n");
+
+    printf("<p><category><b>Categoria:</b> %s</category></p>\n", category);
+
+    printf("</div>\n");
+
+    printf("<text>\n");
+
+    printf("<div style='float: left; width: 50");
+    printf("%");
+    printf(";'>\n");
+    printf("<div style='padding: 20px'>\n");
+    guint length = g_list_length(a->lines);
+    int i = 0;
+
+    for (l = a->lines; l != NULL; l = l->next)
+    {
+        tmp = g_string_free(l->data, FALSE);
+        printf("<p align='justify'>%s</p>\n", tmp);
+
+        if (i == length / 2)
+        {
+            printf("</div>");
+            printf("</div>\n");
+
+            printf("<div style='float: left; width: 49");
+            printf("%");
+            printf(";'>\n");
+            printf("<div style='padding: 20px'>\n");
+        }
+
+        g_free(tmp);
+        i++;
+    }
+
+    printf("</div>");
+    printf("</div>\n");
+
+    printf("</text>\n");
+
+    printf("</pub>\n");
+
+    printf("</body>\n");
+    printf("</html>");
 }
 
 void addId(Angola a, char* str){
@@ -136,14 +184,12 @@ void addCategory(Angola a, char* str){
     g_string_append(a->category,str);
 }
 
-void addText(Angola a, char* str){
-    g_string_append(a->text,str);
+void addLine(Angola a, char* str){
+    a->lines = g_list_append(a->lines, g_string_new(str));
 }
 
 void tag(){
-    
     taghtable = g_hash_table_new_full(g_string_hash, g_string_equal, stringTotalFree, g_hash_table_destroy );
-
 }
 
 void title_index_function(gpointer key, gpointer value, gpointer user_data){
@@ -184,13 +230,13 @@ void tag_index_function(gpointer key, gpointer value, gpointer user_data){
 
 
 void trace(){
-    
+
     printf("<pub id=\"tags\">\n");
     g_hash_table_foreach(taghtable,tag_index_function,NULL);
     printf("</pub>\n");
 
     g_hash_table_foreach(taghtable,title_index_function,NULL);
-    
+
     g_hash_table_destroy(taghtable);
-    
+
 }
